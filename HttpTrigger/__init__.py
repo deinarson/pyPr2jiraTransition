af8c900a11d9cc57464e2261469efc4b463ac4f7
Dale Einarson <dale.einarson@cbc.ca>
2019-01-09 15:07:41 -0500

    hacking

--- HttpTrigger/__init__.py
+++ HttpTrigger/__init__.py
@@ -2,6 +2,15 @@ import logging
 
 import azure.functions as func
 
+from azure.keyvault import KeyVaultClient, KeyVaultAuthentication
+from azure.common.credentials import ServicePrincipalCredentials
+
+credentials = None
+
+import http.client as http_client
+http_client.HTTPConnection.debuglevel = 1
+
+import requests
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -14,11 +23,18 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             pass
         else:
             name = req_body.get('name')
+    
 
     if name:
         return func.HttpResponse(f"Hello {name}!")
     else:
         return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
+             "Please pass a name on the query string or in the request body" + get_json(),
              status_code=400
         )
+
+
+
+def get_json():
+    r = requests.get('https://github.com/timeline.json')
+    return str(r.json())
